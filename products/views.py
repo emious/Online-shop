d2f753eee70cb4a8bf6939f013bde66c3cdd6837
emran bolouri <emransix@gmail.com>
2024-12-27 17:27:51 +0330

    feat(products): add you may like it in product detail

--- products/views.py
+++ products/views.py
@@ -3,12 +3,14 @@ from lib2to3.fixes.fix_input import context
 from products.form import ProductReviewForm
 from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product, ProductReview, Category
+from django.db.models import QuerySet
 
 def product_detail_view(request, product_id):
     single_product = get_object_or_404(Product, id=product_id)
     main_image = single_product.images.filter(is_main=True).first()
     related_products = single_product.get_related_products().prefetch_related('images')[:5]
     reviews = ProductReview.objects.filter(product=single_product).order_by('-created_at')
+    random_products = Product.objects.filter(category_id=single_product.category).order_by("?")[:4]
 
     review_form = None
     if request.user.is_authenticated:
@@ -31,6 +33,7 @@ def product_detail_view(request, product_id):
                   'main_image': main_image,
                    'reviews': reviews,
                    'review_form': review_form,
+                   'random_products':random_products,
                    })
 
 def product_list_view(request,  category_slug):
